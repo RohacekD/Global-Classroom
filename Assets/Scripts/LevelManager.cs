bd5d7f35b8d08f6e3c5128e49d593939f8820a3d
Dominik Roháček <RohacekD@gmail.com>
2017-04-07 19:40:44 +0100

    Menus scene

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadScene(string name)
+    {
+        Application.LoadLevel(name);
+    }
+    
+    public void OpenSubmenu(string name)
+    {
+        
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
