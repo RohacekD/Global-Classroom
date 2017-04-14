caf7f6399144c03814d0583907c081e49af251e3
Dominik Roháček <RohacekD@gmail.com>
2017-04-14 14:01:54 +0100

    Menu functional

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,17 +1,61 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class LevelManager : MonoBehaviour {
 
-	public void LoadScene(string name)
+    Vector3 currentPosition;
+    public GameObject mainMenu;
+    public GameObject highscore;
+    public GameObject playmenu;
+    public GameObject settingsMenu;
+
+    private void hideSubmenus()
+    {
+        GameObject[] submenus = GameObject.FindGameObjectsWithTag("Submenu");
+        foreach (GameObject submenu in submenus)
+        {
+            submenu.SetActive(false);
+        }
+    }
+
+    public void LoadScene(string name)
     {
         Application.LoadLevel(name);
     }
-    
-    public void OpenSubmenu(string name)
+
+    public void BackToMainMenu()
     {
-        
+        mainMenu.SetActive(true);
+        hideSubmenus();
+    }
+
+    public void openSettings()
+    {
+        hideSubmenus();
+        mainMenu.SetActive(false);
+        settingsMenu.SetActive(true);
+    }
+
+    public void openPlayMenu()
+    {
+        hideSubmenus();
+        mainMenu.SetActive(false);
+        playmenu.SetActive(true);
+    }
+
+    public void openHighscore()
+    {
+        hideSubmenus();
+        mainMenu.SetActive(false);
+        highscore.SetActive(true);
+    }
+
+    public void playSinglePlayer()
+    {
+        EditorUtility.DisplayDialog("Missing scene on GIT", "Once we have scene to load place here", "OK");
+        //Application.LoadLevel("Scene");
     }
 
     public void QuitGame()
