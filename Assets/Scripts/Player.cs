f3e45effe674f3d379e441de92fa1b09a6051fdb
Dominik Roháček <RohacekD@gmail.com>
2017-04-22 18:56:32 +0100

    Bombs added

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,10 +9,18 @@ public class Player : MonoBehaviour {
     public float maxHealth { get; set; }
     public string playerName;
     public int score { get; set; }
+    public int bombs {
+        get { return bombs; }
+        set {
+            bombs = value;
+            updateBombsText();
+        }
+    }
 
     public Slider healthBar;
     public Text scoreDisplay;
     public Text nameDisplay;
+    public Text playerBombs;
 
     // Use this for initialization
     void Start () {
@@ -23,6 +31,19 @@ public class Player : MonoBehaviour {
         scoreDisplay.text = score.ToString();
         nameDisplay.text = playerName;
 	}
+
+    private void updateBombsText()
+    {
+        playerBombs.text = bombs.ToString() + "x";
+        if (bombs == 0)
+        {
+            playerBombs.color = Color.red;
+        }
+        else
+        {
+            playerBombs.color = new Color(50.0f / 255.0f, 50.0f / 255.0f, 50.0f / 255.0f, 1f);
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
