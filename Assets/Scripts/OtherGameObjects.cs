d3e7ce986882890119aebd11c1c58cb97ae13884
WooHyowon <djswpsk1024@gmail.com>
2017-04-27 21:38:10 +0100

    Just add c# scripts
    Just add c# scripts

---
+++ Assets/Scripts/OtherGameObjects.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OtherGameObjects : MonoBehaviour
+{
+
+	bool isPlayerOnBase = false;
+
+	// Update is called once per frame
+	void Update () {
+		if (isPlayerOnBase) {
+
+		}
+	}
+
+	// Reload bullets and bombs
+	void Reload () {
+
+	}
+
+	// Make the health(hp) full
+	void FullHealth () {
+	}
+
+	// Make the engine full
+	void FullEngine () {
+	}
+
+}
+
