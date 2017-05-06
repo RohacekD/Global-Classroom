bf57e29142e513d4cdb1afa959a5f50328b1f6a0
WooHyowon <djswpsk1024@gmail.com>
2017-05-06 04:04:41 +0100

    Update building
    Update building

---
+++ Assets/Scripts/BuildingController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingController : MonoBehaviour {
+	
+	public GameObject building;
+	public Vector3 spawnValues;
+	public int buildingCount;
+	public float spawnWait;
+	public float startWait;
+	public float waveWait;
+
+	void Start ()
+	{
+		StartCoroutine (SpawnWaves ());
+	}
+
+	IEnumerator SpawnWaves ()
+	{
+			for (int i = 0; i < 400; i += 40)
+			{
+				Vector3 spawnPosition = new Vector3 (spawnValues.x, 0, spawnValues.z + i);
+				Quaternion spawnRotation = Quaternion.identity;
+				Instantiate (building, spawnPosition, building.transform.rotation);
+				yield return new WaitForSeconds (spawnWait);
+			}
+	}
+}
